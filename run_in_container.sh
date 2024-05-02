2a6e60d845e5487abc352c0f2c13f5b3f5455543
Sander Vocke <sandervocke@gmail.com>
2024-05-02 23:14:39 +0200

    wip

--- run_in_container.sh
+++ run_in_container.sh
@@ -5,6 +5,10 @@ case "$1" in
 	--shell) shell=$2; shift; shift;;
 esac
 
+if [ -f $2 ]; then
+    podman cp $2 $__RUNNING_CONTAINER:$2
+fi
+
 cmd="$shell \"$@\""
 
 echo "Running in container $__RUNNING_CONTAINER: $cmd"

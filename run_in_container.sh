d2809fe20e99fef6bf3121402d49647548cce3ba
Sander Vocke <sandervocke@gmail.com>
2024-05-02 15:23:20 +0200

    wip

--- run_in_container.sh
+++ run_in_container.sh
@@ -1,2 +1,9 @@
-echo "Running in container $__RUNNING_CONTAINER: $@"
-podman exec $__RUNNING_CONTAINER /bin/sh -eo pipefail "$@"
+shell="/bin/sh -eo pipefail"
+case "$1" in
+	--shell) shell=$2; shift; shift;;
+esac
+
+cmd="$shell \"$@\""
+
+echo "Running in container $__RUNNING_CONTAINER: $cmd"
+podman exec $__RUNNING_CONTAINER $cmd

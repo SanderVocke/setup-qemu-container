b9c077711fb6f32098370839a18c1222d61acb6b
Sander Vocke <sandervocke@gmail.com>
2024-05-02 15:28:27 +0200

    wip

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 readonly SCRIPT_PATH=$(readlink -f "$0")
 readonly SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-install -Dv -m755 "$SCRIPT_DIR"/run_in_container.sh abin/run_in_container.sh
+install -Dv -m755 "$SCRIPT_DIR"/run_in_container.sh $pwd/abin/run_in_container.sh
+echo "$pwd/abin" >> $GITHUB_PATH

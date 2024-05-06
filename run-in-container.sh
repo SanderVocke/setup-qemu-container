ca1483f2eafc56e0ad979f2a014b41c2682ee36a
Sander Vocke <sandervocke@gmail.com>
2024-05-06 22:25:05 +0200

    run as root

--- run-in-container.sh
+++ run-in-container.sh
@@ -9,25 +9,25 @@ esac
 # Copy the shell script, if any, into the container at same path
 if [ -f $1 ]; then
     echo "Copying $1 to container"
-	podman exec $__RUNNING_CONTAINER mkdir -p $(dirname $1)
-    podman cp $1 $__RUNNING_CONTAINER:$1
+	$__PODMAN_CMD exec $__PODMAN_ARGS $__RUNNING_CONTAINER mkdir -p $(dirname $1)
+    $__PODMAN_CMD cp $__PODMAN_ARGS $1 $__RUNNING_CONTAINER:$1
 fi
 
 # Delete past temporary files for GITHUB_OUTPUT and GITHUB_ENV
 OUT_FILE=/tmp/_gha_output
 ENV_FILE=/tmp/_gha_env
-podman exec $__RUNNING_CONTAINER /bin/sh -c "rm -f $OUT_FILE $ENV_FILE && touch $OUT_FILE && touch $ENV_FILE"
+$__PODMAN_CMD exec $__PODMAN_ARGS $__RUNNING_CONTAINER /bin/sh -c "rm -f $OUT_FILE $ENV_FILE && touch $OUT_FILE && touch $ENV_FILE"
 
 # Run the command/script
 cmd="$shell $@"
 echo "Running in container $__RUNNING_CONTAINER: $cmd"
-podman exec -e GITHUB_OUTPUT=$OUT_FILE -e GITHUB_ENV=$ENV_FILE -w $GITHUB_WORKSPACE $__RUNNING_CONTAINER $cmd
+$__PODMAN_CMD exec $__PODMAN_ARGS -e GITHUB_OUTPUT=$OUT_FILE -e GITHUB_ENV=$ENV_FILE -w $GITHUB_WORKSPACE $__RUNNING_CONTAINER $cmd
 
 # Propagate GITHUB_OUTPUT and GITHUB_ENV back out
 LOCAL_OUT=$(mktemp)
 LOCAL_ENV=$(mktemp)
-podman cp $__RUNNING_CONTAINER:$OUT_FILE $LOCAL_OUT
-podman cp $__RUNNING_CONTAINER:$ENV_FILE $LOCAL_ENV
+$__PODMAN_CMD cp $__PODMAN_ARGS $__RUNNING_CONTAINER:$OUT_FILE $LOCAL_OUT
+$__PODMAN_CMD cp $__PODMAN_ARGS $__RUNNING_CONTAINER:$ENV_FILE $LOCAL_ENV
 for line in $(cat $LOCAL_OUT); do
   echo "$line" >> $GITHUB_OUTPUT
 done
